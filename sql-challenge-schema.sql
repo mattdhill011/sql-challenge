2adda67665371b2bdf1379e941f780a78c94b4ff
Matthew Hill <101017377+mattdhill011@users.noreply.github.com>
2022-05-25 18:19:30 -0400

    added table schema

---
+++ sql-challenge-schema.sql
@@ -0,0 +1,40 @@
+﻿CREATE TABLE titles (
+	title_id VARCHAR(30) PRIMARY KEY,
+	title VARCHAR(30)
+);
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title VARCHAR(30) NOT NULL,
+	birth_date VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(30) NOT NULL,
+	FOREIGN KEY (emp_title) REFERENCES titles(title_id)
+); 
+
+CREATE TABLE departments (
+	dept_no VARCHAR(30) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(30) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
