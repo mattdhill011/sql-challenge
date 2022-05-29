406b6c76ed8eb9a4cbf636d194f547002e638c5b
Matthew Hill <101017377+mattdhill011@users.noreply.github.com>
2022-05-29 18:24:38 -0400

    finished project

--- EmployeeSQL/sql-challenge-schema.sql
+++ EmployeeSQL/sql-challenge-schema.sql
@@ -23,18 +23,21 @@ CREATE TABLE dept_manager (
 	dept_no VARCHAR(30) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+	
 );
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(30) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
-	emp_no INT NOT NULL,
+	emp_no INT PRIMARY KEY NOT NULL,
 	salary INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
